bb3b7bae0d98abf3e78fc4ab3f6e7007eccf3766
Kyle Rockman <kyle.rockman@mac.com>
2014-04-04 14:42:07 -0500

    adding the jenkins build script

---
+++ .jenkins.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#Setup PYENV
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
+pyenv shell
+pyenv virtualenv pyul#${BUILD_NUMBER}-${PYENV_VERSION}
+pyenv activate "pyul#${BUILD_NUMBER}-${PYENV_VERSION}"
+
+#pip install to virtual env
+pip install -r requirements.txt
+pip install -r test_requirements.txt
+
+#run tests with setup.cfg controlling options to nose
+python setup.py nosetests
