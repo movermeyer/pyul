110ea28097d02bac93b520b6b19c8ad80f8598cf
Kyle Rockman <kyle.rockman@mac.com>
2014-04-07 12:00:56 -0500

    adding a check for success on upload, if not then try register, if still not then fail

--- .jenkins_release.sh
+++ .jenkins_release.sh
@@ -11,3 +11,12 @@ pip install --index-url http://pypi.mapmyfitness.com/mmf/stable/+simple/ pip==1.
 pip install wheel
 
 python setup.py sdist bdist_wheel upload -r mmfpypi
+# Confirm that it worked
+if (( $? )); then
+    #Try register and then upload
+    python setup.py sdist bdist_wheel register upload -r mmfpypi
+    if (( $? )); then
+        echo "Unable to distribute your release!" >&2
+        exit 1
+    fi
+fi
