b1a52f6df9c00a0b638c1e1158375c49a149fd2c
Kyle Rockman <kyle.rockman@mac.com>
2014-04-24 12:39:55 -0500

    updating jenkins release script

--- .jenkins_release.sh
+++ .jenkins_release.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
-export PYENV_ROOT="$HOME/.pyenv"
-export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init -)"
-pyenv shell
-pyenv virtualenv pyul_release#${BUILD_NUMBER}-${PYENV_VERSION} -f
-pyenv activate "pyul_release#${BUILD_NUMBER}-${PYENV_VERSION}"
 
+# pip install the versions of setuptools pip and wheel we need to release the package
 pip install --index-url http://pypi.mapmyfitness.com/mmf/stable/+simple/ setuptools==0.9.8
 pip install --index-url http://pypi.mapmyfitness.com/mmf/stable/+simple/ pip==1.5.2
 pip install wheel
