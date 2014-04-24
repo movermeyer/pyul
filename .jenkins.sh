f885ddc963fc272d97f6676c6a3ad4fcfed36c5a
Kyle Rockman <kyle.rockman@mac.com>
2014-04-24 12:27:31 -0500

    updating build script

--- .jenkins.sh
+++ .jenkins.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-#Setup PYENV
-export PYENV_ROOT="$HOME/.pyenv"
-export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init -)"
-pyenv shell
-pyenv virtualenv pyul#${BUILD_NUMBER}-${PYENV_VERSION} -f
-pyenv activate "pyul#${BUILD_NUMBER}-${PYENV_VERSION}"
-
 #pip install to virtual env
 pip install -r requirements.txt
 pip install -r test_requirements.txt
