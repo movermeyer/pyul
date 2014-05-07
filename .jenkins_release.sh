5c417815628c95402e6db2210787a3ad10f37421
Kyle Rockman <kyle.rockman@mac.com>
2014-05-07 08:25:27 -0500

    updating the release script for home server release

--- .jenkins_release.sh
+++ .jenkins_release.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-# pip install the versions of setuptools pip and wheel we need to release the package
-pip install --index-url http://pypi.mapmyfitness.com/mmf/stable/+simple/ setuptools==0.9.8
-pip install --index-url http://pypi.mapmyfitness.com/mmf/stable/+simple/ pip==1.5.2
+pip install setuptools==0.9.8
+pip install pip==1.5.2
 pip install wheel
 
-# I cannot find anything wrong with calling register for each upload
-python setup.py register -r mmfpypi sdist bdist_wheel upload -r mmfpypi
-python setup.py register -r rocktavious_pypi sdist bdist_wheel upload -r rocktavious_pypi
+python setup.py register sdist bdist_wheel upload
