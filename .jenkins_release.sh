55b852864dd144de52dd48398fe5b91dd85597bc
Kyle Rockman <kyle.rockman@mac.com>
2014-06-16 15:43:43 -0500

    fixing up the release script

--- .jenkins_release.sh
+++ .jenkins_release.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-pip install setuptools==0.9.8
-pip install pip==1.5.2
-pip install wheel
-
-python setup.py register sdist bdist_wheel upload
+python setup.py register -r mmfpypi sdist bdist_wheel upload -r mmfpypi
+python setup.py register -r rocktavious_pypi sdist upload -r rocktavious_pypi
