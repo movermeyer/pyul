fbefcbbb69454072e067d330d6e456ace2dd69ff
Kyle Rockman <kyle.rockman@mac.com>
2014-06-24 09:04:22 -0500

    bumping version, common utils added and enum change to better support getting key by value and vice versus

--- pyul/coreUtils/common.py
+++ pyul/coreUtils/common.py
@@ -89,3 +89,26 @@ def synthesize(inst, name, value, readonly=False):
                                     fset=getattr(member, 'fset', None) or getattr(cls, setterName, None) or customSetter,
                                     fdel=getattr(member, 'fdel', None) or getattr(cls, getterName, None)))
 
+
+def is_descriptor(obj):
+    """Returns True if obj is a descriptor, False otherwise."""
+    return (
+            hasattr(obj, '__get__') or
+            hasattr(obj, '__set__') or
+            hasattr(obj, '__delete__'))
+
+
+def is_dunder(name):
+    """Returns True if a __dunder__ name, False otherwise."""
+    return (name[:2] == name[-2:] == '__' and
+            name[2:3] != '_' and
+            name[-3:-2] != '_' and
+            len(name) > 4)
+
+
+def is_sunder(name):
+    """Returns True if a _sunder_ name, False otherwise."""
+    return (name[0] == name[-1] == '_' and
+            name[1:2] != '_' and
+            name[-2:-1] != '_' and
+            len(name) > 2)
--- pyul/coreUtils/enum.py
+++ pyul/coreUtils/enum.py
@@ -16,17 +16,28 @@ Usage Example
     2
     
 """
+from functools import partial
 __all__ = ['Enum']
+
+def has_key(enum, key):
+    return key in enum.keys()
+
+def get_key(enum, value):
+    keys = [k for k, v in enum.iteritems() if v == value]
+    if len(keys) > 1:
+        raise ValueError('Multiple keys have value {0}, this method is unusable'.format(value))
+    return keys[0]
 
 def Enum(*enumerated):
     #If only a dictionary is passed to enum it means we are making up our own values
     #otherwise just enumerate the given list of keys
     if len(enumerated) == 1 and isinstance(enumerated[0], dict):
         enums = enumerated[0]
-        enums["names"] = enumerated[0].keys()
     else:
         enums = dict(zip(enumerated, range(len(enumerated))))
-        enums["names"] = enumerated
+    
+    enums['get_key'] = partial(get_key, enums)
+    enums['has_key'] = partial(has_key, enums)
     return type('Enum', (), enums)
 
 if __name__ == "__main__":
