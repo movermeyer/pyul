d4d5da53a25b35fc4af4ebe7dabe8408f0e4b119
Kyle Rockman <kyle.rockman@mac.com>
2014-07-24 11:33:09 -0500

    6th try

--- jenkins/virtualenv.sh
+++ jenkins/virtualenv.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-venv_name=.virtualenv
-virtualenv_activate=./${venv_name}/bin/activate
-
 # Check to see if pip and virtualenv is installed
 hash pip 2>/dev/null || { echo >&2 "I require foo but it's not installed.  Aborting."; exit 1; }
 
 hash pip 2>/dev/null || { echo >&2 "Unable to find pip.  Aborting."; exit 1; }
 hash virtualenv 2>/dev/null || { echo >&2 "Unable to find virtualenv.  Aborting."; exit 1; }
+
+venv_name=.virtualenv
+virtualenv_activate=./${venv_name}/bin/activate
 
 # Validate the virtualenv and activate it
 if [[ ! -e $virtualenv_activate ]]
