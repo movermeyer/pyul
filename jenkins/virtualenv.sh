1697201ac0efb8b55a09f0da85ccb9439280d179
Kyle Rockman <kyle.rockman@mac.com>
2014-09-04 10:47:22 -0500

    getting install to work properly
    Signed-off-by: Kyle Rockman <kyle.rockman@mac.com>

--- jenkins/virtualenv.sh
+++ jenkins/virtualenv.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-venv_name=.virtualenv
 virtualenv_activate=./${venv_name}/bin/activate
 
 # Validate the virtualenv and activate it
@@ -9,8 +7,9 @@ then
 fi
 . ${virtualenv_activate}
 
-pip install -r dev-requirements.txt --find-links="./"
+pip install pbr==0.11.0.dev19.gb0cedad --find-links "./"
+pip install -r dev-requirements.txt --find-links "./"
 if [[ -e "./setup.py" ]]
 then
-    python setup.py develop --no-deps
+    python setup.py develop
 fi
--- setup.py
+++ setup.py
@@ -1,10 +1,25 @@
-import sys, setuptools
+import sys
+from setuptools import setup
+from subprocess import call
+from setuptools.command.install import install as _install
+from setuptools.command.develop import develop as _develop
+
+class install(_install):
+    def run(self):
+        call(['pip install -r requirements.txt --find-links "./"'], shell=True)
+        _install.run(self)
+        
+class develop(_develop):
+    def run(self):
+        call(['pip install -r dev-requirements.txt --find-links "./"'], shell=True)
+        _develop.run(self)
 
 kw = {}
 if sys.version_info >= (3,):
     kw['use_2to3'] = True
 
-setuptools.setup(
-    setup_requires=['pbr==0.11.0.dev19.gb0cedad'],
-    pbr=True,
-    **kw)
+setup(cmdclass={'install': install,
+                'develop': develop},
+      setup_requires=['pbr==0.11.0.dev19.gb0cedad'],
+      pbr=True,
+      **kw)
