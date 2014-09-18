83cf996fc13629d1a238aabbafb392a4f4dd264d
Kyle Rockman <kyle.rockman@mac.com>
2014-09-18 09:09:13 -0500

    ok pbr python setup.py develop works, plus install
    Signed-off-by: Kyle Rockman <kyle.rockman@mac.com>

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ try:
     setup(**util.cfg_to_args())
 except ImportError:
     #If PBR isn't installed, then install it from the local egg and redo the python setup.py call
-    call(['easy_install ./pbr-0.11.0.dev33.g572e59e-py2.7.egg'], shell=True)
+    call(['easy_install ./pbr-0.11.0.dev35.g0f936a0-py2.7.egg'], shell=True)
     call([' '.join(['python'] + sys.argv)], shell=True)
 
 
