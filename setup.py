6301d6b5820dd893211685293c18db97fd682675
Kyle Rockman <kyle.rockman@mac.com>
2014-09-18 11:21:52 -0500

    final pbr update hopefully
    Signed-off-by: Kyle Rockman <kyle.rockman@mac.com>

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ try:
     setup(**util.cfg_to_args())
 except ImportError:
     #If PBR isn't installed, then install it from the local egg and redo the python setup.py call
-    call(['easy_install ./pbr-0.11.0.dev37.gbad9819-py2.7.egg'], shell=True)
+    call(['easy_install ./pbr-0.11.0.dev38.g0952dc0-py2.7.egg'], shell=True)
     call([' '.join(['python'] + sys.argv)], shell=True)
 
 
